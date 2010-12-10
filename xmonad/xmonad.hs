158f9b0723ea0290a5feb13d8170f82f18ccbcc1
daoo <daoo314@gmail.com>
2010-12-10 17:24:56 +0100

    Got rid of dmenu.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -19,6 +19,8 @@ import XMonad.Layout.Named
 import qualified XMonad.Layout.NoBorders as B
 import XMonad.Layout.PerWorkspace
 import XMonad.Layout.Reflect
+import XMonad.Prompt
+import XMonad.Prompt.Shell
 import qualified XMonad.StackSet as W
 import XMonad.Util.Run
 import XMonad.Util.Scratchpad 
@@ -56,11 +58,11 @@ myLayoutHook = onWorkspace "[im]" (B.noBorders $ avoidStruts $ myIMLayout) $
 -- Log Hook
 myLogHook :: Handle -> X ()
 myLogHook h   = dynamicLogWithPP $ defaultPP
-  { ppUrgent  = c2 redColor . dzenStrip
-  , ppCurrent = c1 greenColor
-  , ppVisible = c1 blueColor
-  , ppHidden  = c1 fgColor . noNSP
-  , ppTitle   = c1 fgColor
+  { ppUrgent  = c2 barRedColor . dzenStrip
+  , ppCurrent = c1 barGreenColor
+  , ppVisible = c1 barBlueColor
+  , ppHidden  = c1 barFgColor . noNSP
+  , ppTitle   = c1 barFgColor
   , ppWsSep   = " "
   , ppSep     = " | "
   , ppOutput  = hPutStrLn h
@@ -71,9 +73,7 @@ myLogHook h   = dynamicLogWithPP $ defaultPP
     noNSP ws = if ws == "NSP" then "" else ws
 
 -- Simple
-myDmenu = "exec `dmenu_path | dmenu -i -fn '" ++ myFont ++ "' -nb '" ++ bgColor ++ "' -nf '" ++ fgColor ++ "' -sb '" ++ greenColor ++ "' -sf '" ++ bgColor ++ "'`"
-
-barDefault = "-fn '" ++ myFont ++ "' -bg '" ++ bgColor ++ "' -fg '" ++ fgColor ++ "'"
+barDefault = "-fn '" ++ barFont ++ "' -bg '" ++ barBgColor ++ "' -fg '" ++ barFgColor ++ "'"
 
 myLeftBar  = "dzen2 -p -ta l -x 0 -y 0 -w 1200 " ++ barDefault
 myRightBar = "conky -c ~/.xmonad/dzen_conkyrc | dzen2 -p -ta r -x 1200 -y 0 -w 720 " ++ barDefault
@@ -83,18 +83,18 @@ myWorkspaces = [ "[im]", "[web]", "[code]", "[code2]", "[other]", "[music]", "[f
 myTerm   = "urxvt"
 myModKey = mod4Mask
 
-myFont = "-misc-fixed-*-*-*-*-10-*-*-*-*-*-*-*"
+barFont = "-misc-fixed-*-*-*-*-10-*-*-*-*-*-*-*"
 
-fgColor    = "#f6f3e8"
-bgColor    = "#242424"
-redColor   = "#e5786d"
-greenColor = "#95e454"
-blueColor  = "#8ac6f2"
+barFgColor    = "#f6f3e8"
+barBgColor    = "#242424"
+barRedColor   = "#e5786d"
+barGreenColor = "#95e454"
+barBlueColor  = "#8ac6f2"
 
 -- Keys
 keysToAdd x = [ ((modMask x, xK_b), withFocused toggleBorder)
               , ((modMask x, xK_z), focusUrgent)
-              , ((modMask x, xK_p), spawn myDmenu)
+              , ((modMask x, xK_p), shellPrompt defaultXPConfig)
               , ((modMask x, xK_quoteleft), scratchpadSpawnActionTerminal myTerm)
               , ((modMask x, xK_section), scratchpadSpawnActionTerminal myTerm)
               , ((modMask x, xK_x), spawn "firefox-nightly")
@@ -106,7 +106,6 @@ keysToAdd x = [ ((modMask x, xK_b), withFocused toggleBorder)
 keysToRemove :: XConfig Layout ->    [((KeyMask, KeySym),X ())]
 keysToRemove x = []
 
-
 main = do
   spawn myRightBar
   d <- spawnPipe myLeftBar
@@ -115,8 +114,8 @@ main = do
     { terminal           = myTerm
     , modMask            = myModKey
     , borderWidth        = 1
-    , normalBorderColor  = fgColor
-    , focusedBorderColor = blueColor 
+    , normalBorderColor  = barFgColor
+    , focusedBorderColor = barBlueColor 
     , workspaces         = myWorkspaces
 
     , manageHook = myManageHook <+> manageDocks <+> scratchpadManageHookDefault
