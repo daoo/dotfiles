291a3d4be2c657c1eeaf2b45bf368a2815b99968
daoo <daoo314@gmail.com>
2011-03-18 16:44:12 +0100

    Added pentadactyl and custom userChrome.css to firefox. Also luakit.

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ BIN="$HOME/bin"
 echo "vim..."
 ln -s "$SOURCE/vim/vimrc" "$DEST/.vimrc"
 ln -s "$SOURCE/vim/gvimrc" "$DEST/.gvimrc"
-ln -s "$SOURCE/vim/" "$DEST/.vim"
+ln -s "$SOURCE/vim" "$DEST/.vim"
 mkdir -p "$SOURCE/vim/tmp"
 
 echo "git..."
@@ -28,8 +28,17 @@ ln -s "$SOURCE/xinitrc" "$DEST/.xinitrc"
 
 echo "firefox..."
 PROF=$(find $DEST/.mozilla/firefox -maxdepth 1 -name '*default*')
+mkdir -p "$PROF/chrome/"
+ln -s "$SOURCE/firefox/chrome/userChrome.css" "$PROF/chrome/userChrome.css"
 ln -s "$SOURCE/firefox/searchplugins" "$PROF/searchplugins"
 ln -s "$SOURCE/firefox/user.js/" "$PROF/user.js"
+
+echo "pentadactyl..."
+ln -s "$SOURCE/firefox/pentadactyl" "$DEST/.pentadactyl"
+ln -s "$SOURCE/firefox/pentadactyl/pentadactylrc" "$DEST/.pentadactylrc"
+
+echo "luakit..."
+ln -s "$SOURCE/luakit" "$DEST/.config/luakit"
 
 echo "bin..."
 for file in $(ls $SOURCE/scripts/); do
