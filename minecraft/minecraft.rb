43e1be9e47cd1b25153924d93f3f0da862e98c59
daoo <daoo314@gmail.com>
2012-02-24 12:31:30 +0100

    More work on the new minecraft launcher
      * Download latest snapshots
      * Update lwjgl

--- minecraft/minecraft.rb
+++ minecraft/minecraft.rb
@@ -1,5 +1,8 @@
+require "date"
 require "fileutils"
-require 'tempfile'
+require "tempfile"
+require "net/http"
+require "rexml/document"
 
 # {{{ Utils
 module Utils
@@ -24,8 +27,13 @@ module Utils
 
   # Unzip the contents of a zip file to a target directory.
   # Overwrites anything in the target directory. Syscalls the unzip program.
-  def Utils.unzip(file, target)
-    system("unzip -oqq #{file} -d #{target}")
+  def Utils.unzip(file, target, filters = ["*"], junk_paths = false)
+    filters = filters.map { |f| "'#{f}'" }.join(" ")
+    if junk_paths
+      system("unzip -joqq #{file} #{filters} -d #{target}")
+    else
+      system("unzip -oqq #{file} #{filters} -d #{target}")
+    end
   end
 
   # Zip the contents of a directory.
@@ -40,29 +48,187 @@ end
 # }}}
 # {{{ Minecraft
 module Minecraft
-  INSTALL_DIR = File.join(ENV["HOME"], "games/minecraft")
   JAVAOPTS    = "-server -Xmx1024M -Xms512M"
-end
 
-module Client
-  include Minecraft
-
+  # ~/.minecraft
   DOT_DIR         = File.join(ENV["HOME"], ".minecraft")
   BIN_DIR         = File.join(DOT_DIR, "bin")
+  NATIVES_DIR     = File.join(BIN_DIR, "natives")
   JAR_FILE        = File.join(BIN_DIR, "minecraft.jar")
   JAR_BACKUP_FILE = File.join(BIN_DIR, "minecraft.jar.bak")
 
+  # install dir
+  INSTALL_DIR  = File.join(ENV["HOME"], "games/minecraft")
   LAUNCHER_JAR = File.join(INSTALL_DIR, "launcher.jar")
   CLIENTS_DIR  = File.join(INSTALL_DIR, "client")
+  SERVERS_DIR  = File.join(INSTALL_DIR, "server")
   MODS_DIR     = File.join(INSTALL_DIR, "mods")
   LOG_FILE     = File.join(INSTALL_DIR, "minecraft.log")
 
-  TMP_DIR = "/tmp/minecraft.jar/"
-  TMP_JAR = "/tmp/tmp-minecraft.jar"
-end
+  # urls, lwjgl
+  LWJGL_RSS = URI.parse("http://sourceforge.net/api/file/index/project-id/58488/mtime/desc/limit/20/rss")
 
-module Server
-  include Minecraft
+  # urls, release versions
+  RELEASE_LAUNCHER_URL = URI.parse("https://s3.amazonaws.com/MinecraftDownload/launcher/minecraft.jar")
+  RELEASE_SERVER_URL   = URI.parse("https://s3.amazonaws.com/MinecraftDownload/launcher/minecraft_server.jar")
+
+  # urls, snapshots, needs an advanced downloading mechanism
+  SNAPSHOT_URL = URI.parse("http://assets.minecraft.net/")
+
+  # tmp
+  TMP_MINECRAFT_DIR = "/tmp/minecraft.jar/"
+  TMP_MINECRAFT_JAR = "/tmp/tmp-minecraft.jar"
+  TMP_LWJGL_DIR     = "/tmp/lwjgl.zip/"
+
+  # lwjgl files
+  LWJGL_JARS = ["jinput.jar", "lwjgl.jar", "lwjgl_util.jar"].map { |jar|
+    File.join(TMP_LWJGL_DIR, jar)
+  }
+  LWJGL_LIBS = ["libjinput-linux64.so", "libjinput-linux.so", "liblwjgl64.so", "liblwjgl.so", "libopenal64.so", "libopenal.so"].map { |lib|
+    File.join(TMP_LWJGL_DIR, lib)
+  }
+
+  # helper files
+  LWJGL_VERSION = File.join(DOT_DIR, "lwjgl-version")
+end
+# }}}
+# {{{ Updater
+module Updater
+  # Parse an xml file for the latest of element.
+  def Updater.get_latest(uri, top_filter, result_regexp, date_item, result_item)
+    data = Net::HTTP.get_response(uri).body
+    doc  = REXML::Document.new(data)
+
+    latest = nil
+    res    = nil
+    doc.elements.each(top_filter) do |e|
+      k = e.elements[result_item].text
+      if k.match(result_regexp) != nil
+        d = Date.parse(e.elements[date_item].text)
+        if latest == nil or d > latest
+          latest = d
+          res    = k
+        end
+      end
+    end
+
+    return res
+  end
+
+  # Connect to assets.minecraft.net and retrive the latest snapshot version.
+  # Can raise Net::HTTP errors.
+  def Updater.get_latest_snapshot()
+    key = Updater.get_latest(
+      Minecraft::SNAPSHOT_URL,
+      "ListBucketResult/Contents",
+      /\d\dw\d\d[a-z]\/$/,
+      "LastModified",
+      "Key")
+
+    # Remove the trailing slash
+    return key[0, key.length - 1]
+  end
+
+  # Get the latest version of lwjgl
+  def Updater.get_latest_lwjgl()
+    return Updater.get_latest(
+      Minecraft::LWJGL_RSS,
+      "rss/channel/item",
+      /lwjgl-[\.0-9]+\.zip\/download$/,
+      "pubDate",
+      "link")
+  end
+
+  # Download an URI to a file.
+  # Raises error if http request fails.
+  def Updater.download(uri, file)
+    Net::HTTP.get_response(uri) do |resp|
+      if resp.is_a?(Net::HTTPSuccess)
+        File.open(file, "w") do |f|
+          f.write(resp.body)
+        end
+      else
+        raise "Could not download file at uri #{uri.to_s}"
+      end
+    end
+  end
+
+  # Download a specific client snapshot.
+  # Returns true if a new snapshot was downloaded.
+  def Updater.download_client_snapshot(snapshot)
+    client_file = File.join(Minecraft::CLIENTS_DIR, snapshot + ".jar")
+    if not File.exists?(client_file)
+      client_uri = URI.join(Minecraft::SNAPSHOT_URL, snapshot + "/minecraft.jar")
+      download(client_uri, client_file)
+
+      return true
+    end
+
+    return false
+  end
+
+  # Download a specific server snapshot.
+  # Returns true if a new snapshot was downloaded.
+  def Updater.download_server_snapshot(snapshot)
+    server_file = File.join(Minecraft::SERVERS_DIR, snapshot + ".jar")
+    if not File.exists?(server_file)
+      server_uri = URI.join(Minecraft::SNAPSHOT_URL, snapshot + "/minecraft_server.jar")
+      download(server_uri, server_file)
+
+      return true
+    end
+
+    return false
+  end
+
+  # Download and update lwjgl if needed.
+  def Updater.download_lwjgl()
+    lwjgl_version = nil
+    if File.exists?(Minecraft::LWJGL_VERSION)
+      File.open(Minecraft::LWJGL_VERSION, "r") do |f|
+        lwjgl_version = f.readline
+      end
+    end
+
+    lwjgl_url = Updater.get_latest_lwjgl()
+    if lwjgl_version == nil or not lwjgl_url.include?(lwjgl_version)
+      # OK, we can download a new version of LWJGL
+      # Carefully extract the version from the URL
+      new_version = lwjgl_url.match(/\d\.\d\.\d/)[0]
+
+      # Download the zip file from sourceforge
+      # Now, one does not simply download a file from sourceforge
+      # We have to follow all redirects
+      uri  = URI.parse(lwjgl_url)
+      resp = Net::HTTP.get_response(uri)
+      while resp.header["location"]
+        uri  = URI.parse(resp.header["location"])
+        resp = Net::HTTP.get_response(uri)
+      end
+
+      Tempfile.open("lwjgl.zip") do |tmp|
+        tmp.write(resp.body)
+
+        # Okay, now we got the zip, extract it
+        FileUtils.rm_rf(Minecraft::TMP_LWJGL_DIR)
+        Utils.unzip(tmp.path, Minecraft::TMP_LWJGL_DIR, ["*.jar", "*.so"], true)
+
+        # Copy the correct files
+        FileUtils.cp(Minecraft::LWJGL_JARS, Minecraft::BIN_DIR)
+        FileUtils.cp(Minecraft::LWJGL_LIBS, Minecraft::NATIVES_DIR)
+
+        FileUtils.rm_rf(Minecraft::TMP_LWJGL_DIR)
+
+        File.open(Minecraft::LWJGL_VERSION, "w") do |f|
+          f.write(new_version)
+        end
+
+        return true
+      end
+    end
+
+    return false
+  end
 end
 # }}}
 # {{{ Dialog
@@ -210,9 +376,21 @@ class SelectManyFilesDialog < Dialog
 end
 # }}}
 
-diag = MenuDialog.new("Welcome!", ["client", "sever", "check for updates"])
-diag.run()
-puts diag.get_exit()
-puts diag.get_result()
+puts Updater.download_lwjgl()
+exit 0
+
+while true do
+  diag = MenuDialog.new("Welcome!", ["client", "sever", "check for updates"])
+  diag.run()
+  if diag.get_exit()
+    action = diag.get_result()
+    if action == "client"
+    elsif action == "server"
+    elsif action == "check for updates"
+    end
+  else
+    break
+  end
+end
 
 # vim: set fdm=marker :
