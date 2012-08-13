a6ef05789458c19eb66db76d3952252d9158873d
daoo <daoo314@gmail.com>
2012-08-13 14:07:43 +0200

    Implement the rest of the state machine

--- powersave/src/main.c
+++ powersave/src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <sys/stat.h>
 
 #include "functions.h"
 #include "settings.h"
@@ -21,38 +22,51 @@ typedef enum save_state_enum {
   FULL, SAVE
 } save_state;
 
+const save_state DEFAULT_SAVE_STATE       = FULL;
+const lid_state DEFAULT_LID_STATE         = OPEN;
+const adapter_state DEFAULT_ADAPTER_STATE = AC;
+
+const char RUN_DIR[]            = "/run/powersave";
 const char ADAPTER_STATE_FILE[] = "/run/powersave/adapter";
 const char LID_STATE_FILE[]     = "/run/powersave/lid";
 const char SAVE_STATE_FILE[]    = "/run/powersave/save";
 
-void get_state(const char* file, void* state) {
-  read_char(file, (int*) state);
+int get_state(const char* file, int def) {
+  int state;
+  if (read_char(file, &state) == READ_SUCCESS)
+    return state;
+
+  return def;
 }
 
 void set_state(const char* file, int state) {
   write_char(file, state);
 }
 
-save_state calculate_new_state(save_state old, adapter_state adpt, lid_state lid) {
+save_state calculate_new_state(adapter_state adpt, lid_state lid) {
   return lid == OPEN && adpt == AC ? FULL : SAVE;
 }
 
 int main(int argc, char const* argv[]) {
-  save_state old = FULL;
-  save_state new = FULL;
+  if (!path_exists(RUN_DIR)) {
+    mkdir(RUN_DIR, 0755);
+  }
 
-  lid_state lid;
-  adapter_state adapt;
+  save_state old = get_state(SAVE_STATE_FILE, DEFAULT_SAVE_STATE);
+  save_state new = old;
+
+  lid_state lid = get_state(LID_STATE_FILE, DEFAULT_LID_STATE);
+  adapter_state adapt = get_state(ADAPTER_STATE_FILE, DEFAULT_ADAPTER_STATE);
 
   if (argc > 1) {
     if (eq(argv[1], "--ac")) {
-      new = calculate_new_state(old, AC, lid);
+      new = calculate_new_state(AC, lid);
     } else if (eq(argv[1], "--battery")) {
-      new = calculate_new_state(old, BATTERY, lid);
+      new = calculate_new_state(BATTERY, lid);
     } else if (eq(argv[1], "--lid-open")) {
-      new = calculate_new_state(old, adapt, OPEN);
+      new = calculate_new_state(adapt, OPEN);
     } else if (eq(argv[1], "--lid-close")) {
-      new = calculate_new_state(old, adapt, CLOSED);
+      new = calculate_new_state(adapt, CLOSED);
     } else if (eq(argv[1], "--power-save")) {
       new = SAVE;
     } else if (eq(argv[1], "--power-full")) {
