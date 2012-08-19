a31541ad8c81fe45f0b8bde8b0434dc8e43c8e31
daoo <daoo314@gmail.com>
2012-08-19 20:45:27 +0200

    Make commandline util easier to use

--- powersave/src/main.cpp
+++ powersave/src/main.cpp
@@ -6,19 +6,39 @@
 using namespace native;
 using namespace std;
 
-int main(int argc, char const* argv[]) {
-  if (argc > 1) {
-    string argv1(argv[1]);
+void print_help() {
+  cout << "Usage: powersave ACTION\n\n";
+  cout << "Availble actions are:\n";
+  cout << "   auto   save power depending on battery status\n";
+  cout << "   print  print the state of all power saving settings\n";
+  cout << "   full   force full power\n";
+  cout << "   save   force power saving\n";
+}
 
-    if (argv1 == "--power-save") {
-      settings_full();
-    } else if (argv1 == "--power-full") {
-      settings_save();
+int main(int argc, char const* argv[]) {
+  if (argc == 2) {
+    const string argv1(argv[1]);
+
+    if (argv1 == "save") {
+      save_power();
+    } else if (argv1 == "full") {
+      full_power();
+    } else if (argv1 == "auto") {
+      if (on_battery())
+        save_power();
+      else
+        full_power();
+    } else if (argv1 == "print") {
+      print_state();
     } else {
-      cerr << "Unknown parameter " << argv1 << "\n";
+      cerr << "Unknown action " << argv1 << "\n";
+      print_help();
+      return 1;
     }
   } else {
-    settings_check();
+    print_help();
+    return 1;
   }
+
   return 0;
 }
