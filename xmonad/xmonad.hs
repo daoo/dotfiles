fef8af054ff6daa58283b538b88e6c7ef63f952a
daoo <daoo314@gmail.com>
2012-11-19 16:20:42 +0100

    Remove Ewmh and ICCCMFocus
    Made Firefox steal focus when another workspace was active, annoying.
    Also Minecraft still breaks as soon as it have lost focus.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -8,8 +8,6 @@ import Keys
 import Data.Map (union)
 
 import XMonad
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.ICCCMFocus
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.UrgencyHook
 import XMonad.Util.Run
@@ -20,13 +18,13 @@ main = do
   spawn conkyCmd
   d <- spawnPipe dzenCmd
 
-  xmonad $ ewmh $ withUrgencyHook NoUrgencyHook $ defaultConfig
+  xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig
     { borderWidth        = 1
     , focusFollowsMouse  = False
     , focusedBorderColor = winBorderFocused
     , keys               = union newKeyMaps . keys defaultConfig
     , layoutHook         = myLayoutHook
-    , logHook            = takeTopFocus <+> myLogHook d
+    , logHook            = myLogHook d
     , manageHook         = myManageHook <+> manageDocks <+> scratchpadManageHookDefault
     , modMask            = myModKey
     , normalBorderColor  = winBorderNormal
