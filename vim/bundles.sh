d4e4565313451cd2caab1dd98f737b779b114165
daoo <daoo314@gmail.com>
2013-09-02 09:28:31 +0200

    Add more git clones

--- vim/bundles.sh
+++ vim/bundles.sh
@@ -21,7 +21,14 @@ elif [[ $1 = yaourt ]]; then
     vim-systemd \
     vim-ultisnips
 elif [[ $1 = clone ]]; then
+  git clone https://github.com/daoo/Mustang2.git ~/.vim/bundle/mustang2
+  git clone https://github.com/daoo/Wombat.git ~/.vim/bundle/wobat
   git clone https://github.com/maxbrunsfeld/vim-yankstack.git ~/.vim/bundle/vim-yankstack
+  git clone https://github.com/t9md/vim-quickhl.git ~/.vim/bundle/vim-quickhl
+  git clone https://github.com/vim-scripts/Cpp11-Syntax-Support.git ~/.vim/bundle/vim-cpp11
+  git clone https://github.com/vim-scripts/alex.vim.git ~/.vim/bundle/vim-alex
+  git clone https://github.com/vim-scripts/coq-syntax.git ~/.vim/bundle/vim-coq
+  git clone https://github.com/vim-scripts/happy.vim.git ~/.vim/bundle/vim-happy
 elif [[ $1 = pull ]]; then
   for dir in ~/.vim/bundle/*; do
     echo $dir
