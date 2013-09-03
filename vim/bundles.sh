51348d3dd3cdfc1dc67cb9be69c48e5bb5a049d1
daoo <daoo314@gmail.com>
2013-09-03 13:00:29 +0200

    Make bundle script more portable

--- vim/bundles.sh
+++ vim/bundles.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 if [[ $1 = yaourt-git ]]; then
   yaourt --noconfirm -S \
@@ -20,15 +20,30 @@ elif [[ $1 = yaourt ]]; then
     vim-surround \
     vim-systemd \
     vim-ultisnips
+elif [[ $1 = clone-all ]]; then
+  git clone https://github.com/bling/vim-airline.git ~/.vim/bundle/airline
+  git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/fugitive
+  git clone https://github.com/godlygeek/tabular.git ~/.vim/bundle/tabular
+  git clone https://github.com/mileszs/ack.vim.git ~/.vim/bundle/ag
+  git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp
+  git clone https://github.com/tikhomirov/vim-glsl.git ~/.vim/bundle/glsl
+  git clone https://github.com/tpope/vim-markdown.git ~/.vim/bundle/markdown
+  git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
+  git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+  git clone https://github.com/tpope/vim-pathogen.git ~/.vim/bundle/pathogen
+  git clone https://github.com/ReekenX/vim-rename2.git ~/.vim/bundle/rename2
+  git clone https://github.com/tpope/vim-repeat.git ~/.vim/bundle/repeat
+  git clone https://github.com/tpope/vim-surround.git ~/.vim/bundle/surround
+  git clone https://github.com/vim-scripts/UltiSnips.git ~/.vim/bundle/ultisnips
 elif [[ $1 = clone ]]; then
   git clone https://github.com/daoo/Mustang2.git ~/.vim/bundle/mustang2
   git clone https://github.com/daoo/Wombat.git ~/.vim/bundle/wombat
-  git clone https://github.com/maxbrunsfeld/vim-yankstack.git ~/.vim/bundle/vim-yankstack
-  git clone https://github.com/t9md/vim-quickhl.git ~/.vim/bundle/vim-quickhl
-  git clone https://github.com/vim-scripts/Cpp11-Syntax-Support.git ~/.vim/bundle/vim-cpp11
-  git clone https://github.com/vim-scripts/alex.vim.git ~/.vim/bundle/vim-alex
-  git clone https://github.com/vim-scripts/coq-syntax.git ~/.vim/bundle/vim-coq
-  git clone https://github.com/vim-scripts/happy.vim.git ~/.vim/bundle/vim-happy
+  git clone https://github.com/maxbrunsfeld/vim-yankstack.git ~/.vim/bundle/yankstack
+  git clone https://github.com/t9md/vim-quickhl.git ~/.vim/bundle/quickhl
+  git clone https://github.com/vim-scripts/Cpp11-Syntax-Support.git ~/.vim/bundle/cpp11
+  git clone https://github.com/vim-scripts/alex.vim.git ~/.vim/bundle/alex
+  git clone https://github.com/vim-scripts/coq-syntax.git ~/.vim/bundle/coq
+  git clone https://github.com/vim-scripts/happy.vim.git ~/.vim/bundle/happy
 elif [[ $1 = pull ]]; then
   for dir in ~/.vim/bundle/*; do
     echo $dir
