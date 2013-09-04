4f457304b8a3494b6a15592e88e8d105ab8475e0
daoo <daoo314@gmail.com>
2013-09-04 12:36:25 +0200

    Sort commands

--- vim/bundles.sh
+++ vim/bundles.sh
@@ -21,17 +21,17 @@ elif [[ $1 = yaourt ]]; then
     vim-systemd \
     vim-ultisnips
 elif [[ $1 = clone-all ]]; then
+  git clone https://github.com/ReekenX/vim-rename2.git ~/.vim/bundle/rename2
   git clone https://github.com/bling/vim-airline.git ~/.vim/bundle/airline
-  git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/fugitive
   git clone https://github.com/godlygeek/tabular.git ~/.vim/bundle/tabular
-  git clone https://github.com/mileszs/ack.vim.git ~/.vim/bundle/ag
   git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp
-  git clone https://github.com/tikhomirov/vim-glsl.git ~/.vim/bundle/glsl
-  git clone https://github.com/tpope/vim-markdown.git ~/.vim/bundle/markdown
+  git clone https://github.com/mileszs/ack.vim.git ~/.vim/bundle/ag
   git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
   git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+  git clone https://github.com/tikhomirov/vim-glsl.git ~/.vim/bundle/glsl
+  git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/fugitive
+  git clone https://github.com/tpope/vim-markdown.git ~/.vim/bundle/markdown
   git clone https://github.com/tpope/vim-pathogen.git ~/.vim/bundle/pathogen
-  git clone https://github.com/ReekenX/vim-rename2.git ~/.vim/bundle/rename2
   git clone https://github.com/tpope/vim-repeat.git ~/.vim/bundle/repeat
   git clone https://github.com/tpope/vim-surround.git ~/.vim/bundle/surround
   git clone https://github.com/vim-scripts/UltiSnips.git ~/.vim/bundle/ultisnips
