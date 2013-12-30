d2a471de6b6d2edc0d226e39f255f4755d28cc80
daoo <daoo314@gmail.com>
2013-12-30 19:28:31 +0100

    Add command for installing pathogen

--- vim/bundles.sh
+++ vim/bundles.sh
@@ -1,42 +1,49 @@
 #!/usr/bin/env bash
 
-dir=${VIMFILES-${HOME}/.vim}/bundle
+vim_dir=${VIMFILES-${HOME}/.vim}
+bundle_dir="$vim_dir/bundle"
 
 if [[ $1 = clone ]]; then
-  git clone https://github.com/ReekenX/vim-rename2.git "$dir/rename2"
-  git clone https://github.com/bling/vim-airline.git "$dir/airline"
-  git clone https://github.com/daoo/Mustang2.git "$dir/mustang2"
-  git clone https://github.com/daoo/Wombat.git "$dir/wombat"
-  git clone https://github.com/godlygeek/tabular.git "$dir/tabular"
-  git clone https://github.com/kien/ctrlp.vim.git "$dir/ctrlp"
-  git clone https://github.com/maxbrunsfeld/vim-yankstack.git "$dir/yankstack"
-  git clone https://github.com/mileszs/ack.vim.git "$dir/ack"
-  git clone https://github.com/milkypostman/vim-togglelist.git "$dir/togglelist"
-  git clone https://github.com/rking/ag.vim.git "$dir/ag"
-  git clone https://github.com/scrooloose/nerdcommenter.git "$dir/nerdcommenter"
-  git clone https://github.com/scrooloose/nerdtree.git "$dir/nerdtree"
-  git clone https://github.com/t9md/vim-quickhl.git "$dir/quickhl"
-  git clone https://github.com/tikhomirov/vim-glsl.git "$dir/glsl"
-  git clone https://github.com/tpope/vim-fugitive.git "$dir/fugitive"
-  git clone https://github.com/tpope/vim-markdown.git "$dir/markdown"
-  git clone https://github.com/tpope/vim-pathogen.git "$dir/pathogen"
-  git clone https://github.com/tpope/vim-repeat.git "$dir/repeat"
-  git clone https://github.com/tpope/vim-surround.git "$dir/surround"
-  git clone https://github.com/vim-scripts/Cpp11-Syntax-Support.git "$dir/cpp11"
-  git clone https://github.com/vim-scripts/UltiSnips.git "$dir/ultisnips"
-  git clone https://github.com/vim-scripts/alex.vim.git "$dir/alex"
-  git clone https://github.com/vim-scripts/coq-syntax.git "$dir/coq"
-  git clone https://github.com/vim-scripts/happy.vim.git "$dir/happy"
-  git clone https://github.com/vim-scripts/matlab.vim.git "$dir/matlab"
+  mkdir -p $bundle_dir
+  git clone https://github.com/ReekenX/vim-rename2.git "$bundle_dir/rename2"
+  git clone https://github.com/bling/vim-airline.git "$bundle_dir/airline"
+  git clone https://github.com/daoo/Mustang2.git "$bundle_dir/mustang2"
+  git clone https://github.com/daoo/Wombat.git "$bundle_dir/wombat"
+  git clone https://github.com/godlygeek/tabular.git "$bundle_dir/tabular"
+  git clone https://github.com/kien/ctrlp.vim.git "$bundle_dir/ctrlp"
+  git clone https://github.com/maxbrunsfeld/vim-yankstack.git "$bundle_dir/yankstack"
+  git clone https://github.com/mileszs/ack.vim.git "$bundle_dir/ack"
+  git clone https://github.com/milkypostman/vim-togglelist.git "$bundle_dir/togglelist"
+  git clone https://github.com/rking/ag.vim.git "$bundle_dir/ag"
+  git clone https://github.com/scrooloose/nerdcommenter.git "$bundle_dir/nerdcommenter"
+  git clone https://github.com/scrooloose/nerdtree.git "$bundle_dir/nerdtree"
+  git clone https://github.com/t9md/vim-quickhl.git "$bundle_dir/quickhl"
+  git clone https://github.com/tikhomirov/vim-glsl.git "$bundle_dir/glsl"
+  git clone https://github.com/tpope/vim-fugitive.git "$bundle_dir/fugitive"
+  git clone https://github.com/tpope/vim-markdown.git "$bundle_dir/markdown"
+  git clone https://github.com/tpope/vim-repeat.git "$bundle_dir/repeat"
+  git clone https://github.com/tpope/vim-surround.git "$bundle_dir/surround"
+  git clone https://github.com/vim-scripts/Cpp11-Syntax-Support.git "$bundle_dir/cpp11"
+  git clone https://github.com/vim-scripts/UltiSnips.git "$bundle_dir/ultisnips"
+  git clone https://github.com/vim-scripts/alex.vim.git "$bundle_dir/alex"
+  git clone https://github.com/vim-scripts/coq-syntax.git "$bundle_dir/coq"
+  git clone https://github.com/vim-scripts/happy.vim.git "$bundle_dir/happy"
+  git clone https://github.com/vim-scripts/matlab.vim.git "$bundle_dir/matlab"
 elif [[ $1 = pull ]]; then
-  for bundle in $dir/*; do
+  for bundle in $bundle_dir/*; do
     echo "$bundle"
     git -C "$bundle" pull --ff-only
   done
+elif [[ $1 = pathogen ]]; then
+  echo "Downloading pathogen..."
+  mkdir -p $vim_dir/autoload
+  curl -Sso "$vim_dir/autoload/pathogen.vim" \
+    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 else
   echo "Usage: bundles.sh COMMAND
 
 Commands:
-  clone clone repos to $dir
-  pull  update repos in $dir"
+  pathogen install pathogen to $vim_dir/autoload
+  clone    clone repos to $bundle_dir
+  pull     update repos in $bundle_dir"
 fi
