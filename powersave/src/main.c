502d98d691324c3bb948fef60db7e79b7d5f1e14
daoo <daoo314@gmail.com>
2014-01-30 08:21:54 +0100

    Print what we are doing

--- powersave/src/main.c
+++ powersave/src/main.c
@@ -3,14 +3,20 @@
 #include <stdio.h>
 #include <string.h>
 
+const char* SAVE_POWER_MSG = "Entering power saving mode.\n";
+const char* FULL_POWER_MSG = "Entering full power mode.\n";
+
+const char* HELP_MSG =
+  "Usage: powersave ACTION\n\n"
+  "Available actions are:\n"
+  "   auto   save power depending on battery status\n"
+  "   full   force full power\n"
+  "   print  print the state of all power saving settings\n"
+  "   save   force power saving\n";
+
 void print_help()
 {
-  printf("Usage: powersave ACTION\n\n"
-         "Available actions are:\n"
-         "   auto   save power depending on battery status\n"
-         "   full   force full power\n"
-         "   print  print the state of all power saving settings\n"
-         "   save   force power saving\n");
+  fputs(HELP_MSG, stdout);
 }
 
 int main(int argc, char const* argv[])
@@ -19,14 +25,19 @@ int main(int argc, char const* argv[])
     const char* cmd = argv[1];
 
     if (strcmp(cmd, "save") == 0) {
+      fputs(SAVE_POWER_MSG, stdout);
       save_power();
     } else if (strcmp(cmd, "full") == 0) {
+      fputs(FULL_POWER_MSG, stdout);
       full_power();
     } else if (strcmp(cmd, "auto") == 0) {
-      if (on_battery())
+      if (on_battery()) {
+        fputs(SAVE_POWER_MSG, stdout);
         save_power();
-      else
+      } else {
+        fputs(FULL_POWER_MSG, stdout);
         full_power();
+      }
     } else if (strcmp(cmd, "print") == 0) {
       print_state();
     } else {
