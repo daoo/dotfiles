88aad26916138d3f04604f4081210b3e10a3517c
daoo <daoo314@gmail.com>
2014-11-06 12:30:55 +0100

    Remove powersave program
    Provide a smaller print script that just prints the state of all
    powersaving related settings. Powersave settings should in most cases be
    done as settings in /etc.

--- powersave/src/files.h
+++
@@ -1,8 +0,0 @@
-#ifndef FILES_H_IQK2JWB9
-#define FILES_H_IQK2JWB9
-
-void file_print(const char* file);
-void file_write(const char* file, const char* str);
-char file_equal(const char* file, const char* str);
-
-#endif /* end of include guard: FILES_H_IQK2JWB9 */
