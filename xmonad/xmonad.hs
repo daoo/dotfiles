dfa00f641597664a4d19fc12e3b6126bcbe7b542
daoo <daoo314@gmail.com>
2014-11-06 15:12:59 +0100

    Remove program launch shortcuts
    Need a better way to launch programs...

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -207,10 +207,6 @@ myKeyMaps !conf = fromList $
 
   , ((myModKey              , xK_F1    ), safeSpawn "/usr/bin/setxkbmap" ["dvpse"] )
   , ((myModKey              , xK_F2    ), safeSpawn "/usr/bin/setxkbmap" ["usaswe"])
-
-  , ((myModKey              , xK_x     ), safeSpawnProg "/usr/bin/firefox")
-  , ((myModKey              , xK_b     ), safeSpawnProg "/usr/bin/gvim"   )
-  , ((myModKey              , xK_m     ), safeSpawnProg "/usr/bin/zathura")
   ]
 
   ++ dvorakMaps myModKey (windows . W.greedyView)
