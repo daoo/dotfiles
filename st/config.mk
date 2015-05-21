6166838cb54cac58e4c55e00b81ea3e458b39256
Daniel Oom <daoo314@gmail.com>
2015-05-21 15:45:41 +0200

    Add build config for st
    Avoids building with debugging stuff.
    
    Signed-off-by: daoo <daoo314@gmail.com>

---
+++ st/config.mk
@@ -0,0 +1,25 @@
+# st version
+VERSION = 0.5
+
+# paths
+PREFIX = /usr/local
+MANPREFIX = ${PREFIX}/share/man
+
+X11INC = /usr/include/X11
+X11LIB = /usr/lib/X11
+
+# includes and libs
+INCS = -I. -I/usr/include -I${X11INC} \
+       `pkg-config --cflags fontconfig` \
+       `pkg-config --cflags freetype2`
+LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 -lutil -lXext -lXft \
+       `pkg-config --libs fontconfig`  \
+       `pkg-config --libs freetype2`
+
+# flags
+CPPFLAGS = -DVERSION=\"${VERSION}\" -D_DEFAULT_SOURCE -D_XOPEN_SOURCE=600
+CFLAGS += -std=c99 ${INCS} ${CPPFLAGS}
+LDFLAGS += ${LIBS}
+
+# compiler and linker
+CC ?= gcc
