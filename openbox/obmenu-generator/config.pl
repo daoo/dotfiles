3238ab359e0e36ecbed78f4bb93dc1fb844635e0
daoo <daoo314@gmail.com>
2015-08-08 14:57:45 +0200

    Update obmenu-generator version

--- openbox/obmenu-generator/config.pl
+++ openbox/obmenu-generator/config.pl
@@ -21,5 +21,5 @@ our $CONFIG = {
   "missing_icon"        => "gtk-missing-image",
   "name_keys"           => ["Name"],
   "terminal"            => "st",
-  "VERSION"             => 0.64,
+  "VERSION"             => 0.65,
 }
