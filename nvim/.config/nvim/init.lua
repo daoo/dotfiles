42a67d6ee57d942a8e3f1081b1925477ef4e7662
daoo <daoo314@gmail.com>
2023-09-17 16:49:02 +0200

    nvim: map code action to <c-space>

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -243,7 +243,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
     vim.keymap.set('n', '<leader>k', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', '<c-space>', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', '<leader>cf', vim.lsp.buf.format, opts)
   end
 })
