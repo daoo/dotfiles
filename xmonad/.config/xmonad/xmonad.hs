4d94c38899ff7681d6dcd34338bdfc9806a65520
Daniel Oom <oom.daniel@gmail.com>
2024-09-27 11:37:44 +0200

    xmonad: switch to slock

--- xmonad/.config/xmonad/xmonad.hs
+++ xmonad/.config/xmonad/xmonad.hs
@@ -160,7 +160,7 @@ myKeyMaps = fromList
   , xK_q ! io exitSuccess
 
   -- Screen locker
-  , xK_b # safeSpawn "physlock" ["-m"]
+  , xK_b # safeSpawnProg "slock"
 
   -- Setting keyboard layout
   -- Note: z and apostrophe is the same physical key in both layouts
