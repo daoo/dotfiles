3904a25fa7191b68bdb58a4e6494d902532a677f
Daniel Oom <oom.daniel@gmail.com>
2024-11-02 14:20:18 +0100

    nvim: configure crates.nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -202,6 +202,21 @@ require('lazy').setup({
         )
       })
     end
+  },
+  {
+    'saecki/crates.nvim',
+    tag = 'stable',
+    event = { "BufRead Cargo.toml" },
+    config = function()
+      require('crates').setup({
+        lsp = {
+          enabled = true,
+          actions = true,
+          completion = true,
+          hover = true,
+        },
+      })
+    end,
   }
 })
 -- }}}
