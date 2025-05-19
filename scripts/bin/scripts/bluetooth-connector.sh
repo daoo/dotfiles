85cb2ad2184ca4617e6ac50ee155744e69c0ab86
Daniel Oom <oom.daniel@gmail.com>
2025-05-19 15:39:16 +0200

    scripts: add bluetooth-connector.sh

---
+++ scripts/bin/scripts/bluetooth-connector.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if ! systemctl is-active --quiet bluetooth.service; then
+  echo "Starting bluetooth.service..."
+  sudo systemctl start bluetooth.service
+  sleep 1
+fi
+
+device="$(bluetoothctl devices | fzf)"
+if [[ -n "$device" ]]; then
+  name="$(cut -d' ' -f 3- <<<"$device")"
+  address="$(cut -d' ' -f 2 <<<"$device")"
+  echo "Powering on bluetooth..."
+  bluetoothctl power on
+  echo "Connecting to $name"
+  bluetoothctl connect "$address"
+fi
