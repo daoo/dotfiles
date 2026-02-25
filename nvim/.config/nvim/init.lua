1c2ff54b51ae3f4bcecf4d6472091f561a7ddd01
Daniel Oom <oom.daniel@gmail.com>
2026-02-25 20:24:46 +0100

    nvim: setup Neotest

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -179,7 +179,27 @@ require('lazy').setup({
         },
       })
     end,
-  }
+  },
+
+  -- Testing
+  {
+    'nvim-neotest/neotest',
+    dependencies = {
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-lua/plenary.nvim',
+      'nvim-neotest/neotest-python',
+      'nvim-neotest/nvim-nio',
+      'nvim-treesitter/nvim-treesitter'
+    },
+    config = function()
+      require('neotest').setup({
+        adapters = { require('neotest-python') }
+      })
+    end,
+    keys = {
+      { '<leader>t', '<cmd>Neotest summary<cr>', desc = 'Neotest' }
+    }
+  },
 })
 -- }}}
 -- {{{ Settings
